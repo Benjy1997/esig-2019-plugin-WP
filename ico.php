88070cec3f5ff11c74352f561657694e2ab75e56
Benjy <smile.2800@hotmail.com>
2019-05-20 10:45:16 +0200

    - Implémentation de l'enregistrement des shortcode. - Ajout des images composant le plugin.

--- ico.php
+++ ico.php
@@ -5,14 +5,17 @@ Plugin Name: Mon plugin ico
 
 
 function mon_plugin_ico_shortcode($atts, $content){
+
+    // Récupère le chemin vers l'image.
     $imgDir = get_site_url().'/wp-content/plugins/ico-plugin/img/';
-    $imgPath = $imgDir.$content;
+    $imgPath = $imgDir.$content.'.png';
 
-
-    $color = 'red';
+    // Initialise les attributs par défaut
+    $color = 'black';
     $padding = '9px';
     $radius = '100%';
 
+    // Modifie les attributs.
     if (isset($atts['color'])){
         $color = $atts['color'];
     }
@@ -25,8 +28,16 @@ function mon_plugin_ico_shortcode($atts, $content){
         $radius = $atts['radius'];
     }
 
-    $html = '<img style="background-color:'.$color.'; padding:'.$padding.'; border-radius:'.$radius.';"'
-          .'src="'.$imgPath.'" alt="wordpress" /%gt;';
+    // Retourne le code HTML.
+    return $html = '<img style="background-color:'.$color.'; padding:'.$padding.'; border-radius:'.$radius.';"'
+          .'src="'.$imgPath.'" alt="wordpress" /%gt;>';
 }
+
+//Enregistre les shortcodes du plugin
+function mon_plugin_ico_register_shortcode() {
+    add_shortcode('ico','mon_plugin_ico_shortcode');
+}
+
+add_action( 'init', 'mon_plugin_ico_register_shortcode' );
 
 
